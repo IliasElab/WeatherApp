126d28c343ac606e69b4a6659751e81d41ea4929
IliasElab <elab.ilias@gmail.com>
2022-11-19 19:51:20 +0100

    Basic Config Files

---
+++ src/Utilites/BasicStyle.tsx
@@ -0,0 +1,12 @@
+const BasicStyle: { [key: string]: any } = {
+  COLORS: {
+    DARKBG: "rgb(29, 44, 101)",
+    DARK: "#1D2C65",
+    LITTLEDARK: "#2F4392",
+    NORMAL: "#4262DA",
+    LIGHT: "#8F8FD5",
+    VERYLIGHT: "#AFABD0",
+  },
+};
+
+export default BasicStyle;
